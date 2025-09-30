9ea534c3283373405510924c9079fd4a8c6f4c14
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-09-30 23:17:22 +0100

    lutando com o main hojeee

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:3000',             // frontend local
-        'https://link-metrics-frontend.vercel.app', // frontend em produção
-      ],
-      credentials: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://link-metrics-frontend.vercel.app',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
-
-  console.log(`🚀 Backend running on port ${port}`);
+  console.log(`🚀 Backend rodando em http://localhost:${port}`);
 }
 bootstrap();
+
 
 
 
